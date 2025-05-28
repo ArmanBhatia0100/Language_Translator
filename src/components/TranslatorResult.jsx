cf425937241a77ca642f7d45775866bbe283d5e8
Arman Bhatia <bhat0100@algonquinlive.com>
2025-05-27 21:59:42 -0400

    fea: text to speach working

--- src/components/TranslatorResult.jsx
+++ src/components/TranslatorResult.jsx
@@ -2,18 +2,24 @@ import TranslatorNavbar from './translatorNavbar'
 import TranslatorResultArea from './TranslatorResultArea'
 import CopyAllIcon from '@mui/icons-material/CopyAll';
 import IconButton from '@mui/material/IconButton';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import { useState } from 'react';
 import { copyToClipboard } from '../services/utils';
 
+const handleSpeak = (text) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'fr-FR'; 
+    window.speechSynthesis.speak(utterance);
+  };
+
 export default function TranslatorResult({translatedText, resultCharacterCount}) {
-    const [characterCount, setCharacterCount] = useState(0);
     return (
         <div className="input-container-one bg-red-200 p-4 rounded-lg w-full md:w-1/2">
             <div className="navbar">
                 <TranslatorNavbar />
             </div>
             <div className="text-area">
-                <TranslatorResultArea translatedText={translatedText} setCharacterCount={setCharacterCount} />
+                <TranslatorResultArea translatedText={translatedText} />
             </div>
             <div className="button-container flex flex-col items-end">
                 <p className='text-sm text-gray-500 mb-2'>{resultCharacterCount}/300</p>
@@ -21,6 +27,9 @@ export default function TranslatorResult({translatedText, resultCharacterCount})
                     <div className='icons-button'>
                         <IconButton aria-label="copy" onClick={() => copyToClipboard(translatedText)}>
                             <CopyAllIcon />
+                        </IconButton>
+                        <IconButton aria-label="speak" onClick={() => handleSpeak(translatedText)}>
+                            <VolumeUpIcon />
                         </IconButton>
                     </div>
 
